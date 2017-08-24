9109abf105447fb4689eefa5f761fd55ad379032
mixnam <mixnam0905@gmail.com>
2017-08-25 01:02:19 +0300

    test

--- echo_bot.py
+++ echo_bot.py
@@ -57,3 +57,7 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
